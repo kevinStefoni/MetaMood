af489cb806c9f633929b367622d49386e2455170
Kevin <kevinStefoni@gmail.com>
2023-03-22 12:13:37 -0500

    Started working on DataTable.razor. Added welcome that fits time when page is accessed.

--- MetaMood/Program.cs
+++ MetaMood/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://kdt48lmf8a.execute-api.us-east-1.amazonaws.com") });
 
 await builder.Build().RunAsync();
---
+++ MetaMood/wwwroot/js/timed-welcome.js
@@ -0,0 +1,28 @@
+﻿window.jsFunctions =
+{
+    TimedWelcome: function () {
+        var today = new Date();
+        var hourNow = today.getHours();
+
+        var greeting;
+        if (hourNow >= 21 || hourNow <= 3)
+        {
+            greeting = 'Good night!';
+        }
+        else if(hourNow >= 18) {
+            greeting = 'Good evening!';
+        }
+        else if (hourNow >= 12) {
+            greeting = 'Good afternoon!';
+        }
+        else if (hourNow > 3) {
+            greeting = 'Good morning!';
+        }
+        else {
+            greeting = 'Welcome!';
+        }
+
+        document.getElementById('greeting').innerHTML = greeting;
+    }
+}
+
