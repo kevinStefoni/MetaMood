fe13cc1e127b4474b25453219acb67b3b80617fd
Kevin <kevinStefoni@gmail.com>
2023-03-24 13:45:19 -0500

    Made navbar work with smaller screens and mobile. Added row hover effect on tables.

--- MetaMood/Program.cs
+++ MetaMood/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://kdt48lmf8a.execute-api.us-east-1.amazonaws.com") });
 
 await builder.Build().RunAsync();
