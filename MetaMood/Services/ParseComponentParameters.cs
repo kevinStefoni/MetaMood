33b777298fca552a0bf6dccf8bb852bb7b755bb0
Kevin <kevinStefoni@gmail.com>
2023-03-28 10:04:46 -0500

    Added pagination buttons that work based on the number of records in table. Made an API call to /count/{table} to get the number of records in the table. Finished the styling for the pagination buttons.

---
+++ MetaMood/Services/ParseComponentParameters.cs
@@ -0,0 +1,23 @@
+﻿using MetaMood.Models;
+
+namespace MetaMood.Services
+{
+    public class ParseComponentParameters
+    {
+
+        /// <summary>
+        /// This method simply returns a string that conveys what type of entity the table will be displaying.
+        /// </summary>
+        /// <param name="type">The type of the entity being displayed in the table</param>
+        /// <returns>The name of the table that is going to be created</returns>
+        public static string? ParseTableFromType(Type? type)
+        {
+
+            if(type == typeof(SpotifyTrackDTO))
+                return "spotify-tracks";
+
+            return null;
+        }
+
+    }
+}
