8586df7164ff09c2480d985c2d4d5a233a2b2edd
Kevin <kevinStefoni@gmail.com>
2023-03-29 13:31:30 -0500

    Chart now displays actual averages.

--- MetaMood/Services/ChartService.cs
+++ MetaMood/Services/ChartService.cs
@@ -1,10 +1,17 @@
-﻿using System.ComponentModel;
+﻿using MetaMood.Models;
+using System.ComponentModel;
+using System.Net.Http.Json;
 
 namespace MetaMood.Services
 {
     public class ChartService
     {
 
+        /// <summary>
+        /// This function uses reflection to get all of the properties of the given type that should make it into the bar chart.
+        /// </summary>
+        /// <param name="type">The type of the model whose data will be graphed</param>
+        /// <returns>An array of strings containing the labels for the X-axis of the graph</returns>
         public static string[] GetLabels(Type? type)
         {
             IList<string> labels = new List<string>();
@@ -14,7 +21,7 @@ namespace MetaMood.Services
                 // add all properties that have double values as labels
                 foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(type))
                 {
-                    if (descriptor.PropertyType == typeof(double?) && !descriptor.DisplayName.Equals("Loudness"))
+                    if (descriptor.PropertyType == typeof(double?) && !descriptor.DisplayName.Equals("Loudness") && !descriptor.DisplayName.Equals("Tempo"))
                     {
                         labels.Add(descriptor.DisplayName);
                     }
