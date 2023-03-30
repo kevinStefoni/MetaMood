0fd9729a2f9ac001ab3329e85f98c98b5e6da21b
Kevin <kevinStefoni@gmail.com>
2023-03-30 17:56:02 -0500

    Created styling for pressing sorting buttons.

---
+++ MetaMood/wwwroot/js/data-table-sorting-buttons.js
@@ -0,0 +1,24 @@
+﻿function SortingButtons(id) {
+
+    var wasActive = document.getElementById(id).classList.contains('table-head-button-active');
+    var otherActiveButtons = document.getElementsByClassName('table-head-button-active');
+
+    for (var i = 0; i < otherActiveButtons.length; i++) {
+        otherActiveButtons[i].classList.add('table-head-button');
+        otherActiveButtons[i].classList.remove('table-head-button-active');
+    }
+
+    if (wasActive) {
+        document.getElementById(id).classList.add('table-head-button');
+        document.getElementById(id).classList.remove('table-head-button-active');
+        document.getElementById('test').innerHTML = "UPPER";
+    }
+    else {
+        document.getElementById(id).classList.add('table-head-button-active');
+        document.getElementById(id).classList.remove('table-head-button');
+        document.getElementById('test').innerHTML = "LOWER";
+    }
+
+}
+
+
