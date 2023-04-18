c74a4ccb9a234bdf55d08e2209911ec55c6cb705
Kevin <kevinStefoni@gmail.com>
2023-04-18 10:58:07 -0500

    Spotify redirect with authorization works.

--- MetaMood/Program.cs
+++ MetaMood/Program.cs
@@ -1,22 +1,10 @@
 using MetaMood;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient(new HttpClientHandler { AllowAutoRedirect = false }) { BaseAddress = new Uri("https://kdt48lmf8a.execute-api.us-east-1.amazonaws.com") });
-
-/*builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyHeader()
-               .AllowAnyMethod();
-    });
-});*/
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://kdt48lmf8a.execute-api.us-east-1.amazonaws.com") });
 
 await builder.Build().RunAsync();
