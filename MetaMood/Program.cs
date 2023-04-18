a3f5782b47c83c49de5083d496f6f92d16f6c458
Kevin <kevinStefoni@gmail.com>
2023-04-18 07:28:55 -0500

    Trying to fix spotify authentication.

--- MetaMood/Program.cs
+++ MetaMood/Program.cs
@@ -1,10 +1,22 @@
 using MetaMood;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://kdt48lmf8a.execute-api.us-east-1.amazonaws.com") });
+
+builder.Services.AddScoped(sp => new HttpClient(new HttpClientHandler { AllowAutoRedirect = false }) { BaseAddress = new Uri("https://kdt48lmf8a.execute-api.us-east-1.amazonaws.com") });
+
+/*builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});*/
 
 await builder.Build().RunAsync();
