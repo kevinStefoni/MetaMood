06452e0fc0b02a136326633b2d32b3fabe4357bc
Kevin <kevinStefoni@gmail.com>
2023-04-18 17:15:33 -0500

    Still working on spotify authentication.

--- MetaMood/Program.cs
+++ MetaMood/Program.cs
@@ -1,4 +1,6 @@
+using Blazored.LocalStorage;
 using MetaMood;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,5 +8,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://kdt48lmf8a.execute-api.us-east-1.amazonaws.com") });
-
+builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
