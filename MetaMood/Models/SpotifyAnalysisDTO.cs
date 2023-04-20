d293ffcf3a0e32fef60be464de703ac77e706aa7
Kevin <kevinStefoni@gmail.com>
2023-04-20 10:35:10 -0500

    Retrieved Reddit data. Started working on sending data to AWS lambda for spotify data analysis. Created new DTO for spotify analysis objects.

---
+++ MetaMood/Models/SpotifyAnalysisDTO.cs
@@ -0,0 +1,23 @@
+﻿using static MetaMood.Models.CustomAttributes.CustomAttributes;
+using System.ComponentModel;
+
+namespace MetaMood.Models
+{
+    public class SpotifyAnalysisDTO
+    {
+
+        public double? Acousticness { get; set; }
+
+        public double? Danceability { get; set; }
+
+        public double? Energy { get; set; }
+
+        public double? Liveness { get; set; }
+
+        public double? Speechiness { get; set; }
+
+        public double? Instrumentalness { get; set; }
+
+        public double? Valence { get; set; }
+    }
+}
--- MetaMood/Services/AllMappersProfile.cs
+++ MetaMood/Services/AllMappersProfile.cs
@@ -13,8 +13,8 @@ namespace MetaMood.Services
         public AllMappersProfile()
         {
 
-            CreateMap<TrackAudioFeatures, SpotifyTrackDTO>();
-            CreateMap<SpotifyTrackDTO, TrackAudioFeatures>().ForMember(t => t.TrackId, opt => opt.Ignore())
+            CreateMap<TrackAudioFeatures, SpotifyAnalysisDTO>();
+            CreateMap<SpotifyAnalysisDTO, TrackAudioFeatures>().ForMember(t => t.TrackId, opt => opt.Ignore())
                                                       .ForMember(t => t.TrackHref, opt => opt.Ignore())
                                                       .ForMember(t => t.Key, opt => opt.Ignore())
                                                       .ForMember(t => t.Mode, opt => opt.Ignore())
@@ -27,9 +27,9 @@ namespace MetaMood.Services
                                                       .ForMember(t => t.Danceability, opt => opt.MapFrom(src => src.Danceability))
                                                       .ForMember(t => t.Energy, opt => opt.MapFrom(src => src.Energy))
                                                       .ForMember(t => t.Liveness, opt => opt.MapFrom(src => src.Liveness))
-                                                      .ForMember(t => t.Loudness, opt => opt.MapFrom(src => src.Loudness))
+                                                      .ForMember(t => t.Loudness, opt => opt.Ignore())
                                                       .ForMember(t => t.Speechiness, opt => opt.MapFrom(src => src.Speechiness))
-                                                      .ForMember(t => t.Tempo, opt => opt.MapFrom(src => src.Tempo))
+                                                      .ForMember(t => t.Tempo, opt => opt.Ignore())
                                                       .ForMember(t => t.Instrumentalness, opt => opt.MapFrom(src => src.Instrumentalness))
                                                       .ForMember(t => t.Valence, opt => opt.MapFrom(src => src.Valence));
 
