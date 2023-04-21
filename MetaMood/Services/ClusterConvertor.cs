4bc67876d2439ac3f8747724f618e327800d4680
Kevin <kevinStefoni@gmail.com>
2023-04-21 18:09:27 -0500

    Converted results from aws lambda to a standard way of storing data. Added loading for each platform.

---
+++ MetaMood/Services/ClusterConvertor.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MetaMood.Services
+{
+    public static class ClusterConvertor
+    {
+
+        public static string ToCluster(this int a)
+        {
+            return a switch
+            {
+                0 => "Happy",
+                1 => "Sad",
+                2 => "Ferocious",
+                3 => "Upbeat Happy",
+                4 => "Upbeat Sad",
+                5 => "Relaxed",
+                6 => "Anger",
+                _ => "ERROR",
+            };
+        }
+
+    }
+}
